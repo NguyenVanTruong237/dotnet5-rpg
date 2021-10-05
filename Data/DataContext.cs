f05f93eee18f6ad00ade00e71e1a25121f4e16d4
Truong10Ngon <toiiuvn013@gmail.com>
2021-10-05 21:25:00 +0700

    Set Up EF

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+using dotnet5_rpg.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet5_rpg.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Character> Characters { get; set; }
+        
+        
+    }
+}
