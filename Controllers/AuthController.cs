d0636a73f494b881d8130cd94ac599976dd20fe0
Truong10Ngon <toiiuvn013@gmail.com>
2021-10-06 21:11:29 +0700

    Controller Register

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using dotnet5_rpg.Data;
+using dotnet5_rpg.Dtos.User;
+using dotnet5_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet5_rpg.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _auRepo;
+        public AuthController(IAuthRepository auRepo)
+        {
+            _auRepo = auRepo;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register (UserRegisterDto request)
+        {
+            var serviceResponse = await _auRepo.Register(
+                new User {UserName = request.UserName}, request.Password
+            );
+            if(!serviceResponse.Success)
+            {
+                return BadRequest(serviceResponse);
+            }
+            return Ok(serviceResponse);
+        }
+    }
+}
