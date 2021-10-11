d56cbe398fa3e552acd6aabfa17ff853f7400445
Truong10Ngon <toiiuvn013@gmail.com>
2021-10-12 00:42:55 +0700

    Role for User, and Claim Role to JWT

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -22,7 +22,8 @@ namespace dotnet5_rpg.Data
                 new Skill{ Id = 2, Name= "Frenzy", Damage = 20},
                 new Skill{ Id = 3, Name= "Blizzard", Damage = 50}
             );
+            modelBuilder.Entity<User>()
+                .Property(user => user.Role).HasDefaultValue("Player");
         }
-
     }
 }
